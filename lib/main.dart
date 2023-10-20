e56ab83f977df873fd09e08fca4e49bed459a285
이관영 <71133204+stufit@users.noreply.github.com>
2023-10-20 15:50:40 +0900

    AppBar, Stack, Align위젯

--- lib/main.dart
+++ lib/main.dart
@@ -4,70 +4,132 @@ void main() {
   runApp(const MyApp());
 }
 
-
+/*
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(
-          primarySwatch: Colors.amber),
-          debugShowCheckedModeBanner: false, // 오른쪽 구석에 debug 표시 제거
-          home: Scaffold(
-            appBar: AppBar(
-              title: Text("AWESOME BRO"),
-            ),
-            body: Center(
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: '아이디',
-                )
-              )
-            ),
-            floatingActionButton: FloatingActionButton(
-              elevation: 0.0, // 그림자 같은거
-              onPressed: () {}, // 익명 함수
-              child: Icon(Icons.add),
-            ),
-            floatingActionButtonLocation: FloatingActionButtonLocation.endFloat, // 더하기 버튼 위치 조정
-            drawer: Drawer(
-              child: ListView(
-                children: [
-                  ListTile(title:Text("첫번째 메뉴")),
-                  ListTile(title:Text("두번째 메뉴")),
-                ],
-              ),
-            ), // 왼쪽 상단 메뉴
-            persistentFooterButtons: [
-              Icon(Icons.settings),
-              SizedBox(width: 20), // 아이콘 사이 간격
-              Icon(Icons.access_alarm),
-              SizedBox(width: 20),
-            ], // 하단 아이콘
-            bottomNavigationBar: BottomNavigationBar(
-              currentIndex: 0, // 어디가 초기값인지
-              fixedColor: Colors.green,
-              items: [
-                BottomNavigationBarItem(
-                    label: "메인메뉴",
-                    icon: Icon(Icons.home)),
-                BottomNavigationBarItem(
-                    label: "검색",
-                    icon: Icon(Icons.search)),
-                BottomNavigationBarItem(
-                    label: "프로필",
-                    icon: Icon(Icons.account_circle)),
-              ],
-              onTap: (int indexOfItem){},
-              backgroundColor: Colors.amber,
-            ),
-            backgroundColor: Colors.indigo,
-            resizeToAvoidBottomInset: false, // 키보드가 올라오면 화면이 줄어드는 것을 방지
+      debugShowCheckedModeBanner: false,
+      home:Scaffold(
+        appBar: AppBar(
+          title: Text("어썸솔로"),
+          centerTitle: false,
+          backgroundColor: Colors.amber,
+          elevation: 10.0,
+          // leading은 제목 왼쪽
+          leading: IconButton(
+            icon:Icon(Icons.menu),
+            tooltip: '메뉴 버튼',
+            onPressed: () {
+              print('메뉴 버튼 클릭됨');
+            },
           ),
-        );
+          // actions 는 제목 오른쪽
+          actions: [
+            IconButton(
+              onPressed: (){},
+              icon: Icon(Icons.comment),
+              tooltip: '설명 버튼',
+            ),
+            IconButton(
+                onPressed: (){},
+                icon: Icon(Icons.settings),
+                tooltip: '설정 버튼',
+            ),
+          ],
+        ),
+      )
+    );
   }
 }
+
+ */
+
+
+/*
+// stack 쌓는것.
+class MyApp  extends StatelessWidget {
+  const MyApp ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Stack(
+            children: [
+              Container(
+                width: 400,
+                height: 400,
+                color: Colors.yellow,
+              ),
+              Container(
+                width: 300,
+                height: 300,
+                color: Colors.blue,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.green,
+              ),
+            ],
+          )
+        ),
+      )
+    );
+  }
+}
+
+ */
+
+
+// positioned
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          body: Center(
+              child: Stack(
+                children: [
+                  Container(
+                    width: 400,
+                    height: 400,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 300,
+                    height: 300,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.green,
+                  ),
+                  Positioned(
+                      top: 50,
+                      left: 50,
+                      child: Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.indigo,
+                      )
+                  )
+                ],
+              )
+          ),
+        )
+    );
+  }
+
+}
+
+
 
 
