1353270d12a1be4de3b5abd47cfcb54c2cc740cf
이관영 <71133204+stufit@users.noreply.github.com>
2023-10-23 15:57:54 +0900

    바로 만들어보기:Music 앱화면 구성하기

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,41 +9,85 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: 400,
-          color: Colors.amber,
-          child: Text("스투핏코딩",
-          textDirection: TextDirection.ltr,
-          style:TextStyle(fontSize: 24) ,
-          overflow: TextOverflow.fade,
-          ),
-        ),
-        Container(
-          width: 400,
-          color: Colors.red,
-          child: Text("호이호이",
-            textDirection: TextDirection.ltr,
-            style:TextStyle(
-              fontFamily: 'cafe',
-                fontSize: 30,
-            ) ,
-            // overflow: TextOverflow.clip,
-          ),
-        ),
-        Container(
-          child: Image(
-            image: AssetImage('assets/images/wallpaper-powfactor.jpg'),
-          ),
-        ),
-        Container(
-          child: Image.network('https://www.fun-coding.org/img/newfunlogo72.png'),
-        )
-      ],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MusicPlayerScreen(),
     );
   }
 }
 
+
+
+class MusicPlayerScreen extends StatelessWidget {
+  const MusicPlayerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          '스투핏 플레이어',
+          style: TextStyle(
+            fontFamily: 'NotoSans',
+            fontSize: 30,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 200,
+                    width: 200,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/coding.jpg'),
+                        fit: BoxFit.cover
+                      )
+                    ),
+                ),
+                SizedBox(height: 20),
+                Text('곡 제목',style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Dohyeon'
+                ),),
+                SizedBox(height: 10),
+                Text('아티스트명',style: TextStyle(
+                  fontSize: 18,
+                  fontStyle: FontStyle.italic,
+                  fontFamily: 'cafe'
+                ),)
+
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24,vertical: 16),
+            child: Column(
+              children: [
+                Slider(value: 0.5, onChanged: (value) {}),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(onPressed: (){}, icon: Icon(Icons.skip_previous)),
+                    IconButton(onPressed: (){}, icon: Icon(Icons.play_arrow)),
+                    IconButton(onPressed: (){}, icon: Icon(Icons.skip_next))
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
 
 
