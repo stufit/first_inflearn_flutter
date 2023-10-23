5e7380e658908a64a81a5935d4bfcd5f6ecce138
이관영 <71133204+stufit@users.noreply.github.com>
2023-10-23 15:08:28 +0900

    Font,Image,Assets 위젯

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,39 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        Container(
+          width: 400,
+          color: Colors.amber,
+          child: Text("스투핏코딩",
+          textDirection: TextDirection.ltr,
+          style:TextStyle(fontSize: 24) ,
+          overflow: TextOverflow.fade,
+          ),
+        ),
+        Container(
+          width: 400,
+          color: Colors.red,
+          child: Text("호이호이",
+            textDirection: TextDirection.ltr,
+            style:TextStyle(
+              fontFamily: 'cafe',
+                fontSize: 30,
+            ) ,
+            // overflow: TextOverflow.clip,
+          ),
+        ),
+        Container(
+          child: Image(
+            image: AssetImage('assets/images/wallpaper-powfactor.jpg'),
+          ),
+        ),
+        Container(
+          child: Image.network('https://www.fun-coding.org/img/newfunlogo72.png'),
+        )
+      ],
+    );
   }
 }
 
