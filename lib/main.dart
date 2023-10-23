0b55f17366b0bd91827edd66bf6d0ddae38f8f07
이관영 <71133204+stufit@users.noreply.github.com>
2023-10-23 21:37:32 +0900

    Stateful Widget 작성 방법

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,32 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _count = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue)),
       home: Scaffold(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        // appBar: AppBar(
-        //   title: Text("로그인 스크린", style: TextStyle(color: Colors.white)),
-        //   backgroundColor: Theme.of(context).colorScheme.primary,
-        // ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                SizedBox(height: 60),
-                Icon(Icons.account_circle, size: 100, color: Colors.white),
-                SizedBox(height: 30),
-                Text(
-                  '어서오세요!',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
-                ),
-                SizedBox(height: 30,),
-                TextField(
-                  decoration: InputDecoration(
-                      labelText: 'Email',
-                      fillColor: Colors.white,
-                      filled: true,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20)
-                      ),
-                      prefixIcon: Icon(Icons.email)),
-                ),
-                SizedBox(height: 16),
-                TextField(
-                  decoration: InputDecoration(
-                      labelText: 'Password',
-                      fillColor: Colors.white,
-                      filled: true,
-                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
-                      prefixIcon: Icon(Icons.lock)),
-                ),
-                SizedBox(height: 24),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      textStyle: TextStyle(fontSize: 20),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20))),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(vertical: 16),
-                    child: Text(
-                      '로그인',
-                      style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Theme.of(context).colorScheme.primary,
-                          fontFamily: "Dohyeon"),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 16),
-                TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    '비밀번호 찾기',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 16),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('Don\'t have an account?',style: TextStyle(fontSize: 16,color: Colors.white),),
-                    TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          '회원가입',
-                          style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white),
-                        ))
-                  ],
-                )
-              ],
+        body: Center(
+            child: ElevatedButton(
+          child: Text('$_count'),
+        onPressed: () {
+            setState(() {
+              _count++;
+            });
+        }
             ),
-          ),
         ),
       ),
     );
