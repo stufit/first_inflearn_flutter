f91e3e9156db7cb1e71522a96d18dc429fd10ae2
이관영 <71133204+stufit@users.noreply.github.com>
2023-10-23 18:26:23 +0900

    바로 만들어보기: 로그인 화면 구성하기

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,95 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: '내앱',
-    theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green)),
-    home: MyApp(),
-  ));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
-
-  final TextEditingController _emailController = TextEditingController();
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue)),
       home: Scaffold(
         appBar: AppBar(
-            title: Text(
-          "스투핏앱",
-        )),
-        body: Center(
-          child: Column(
-            children: [
-              SizedBox(height: 10),
-              TextField(
-                decoration: InputDecoration(
-                    labelText: "Email",
-                    labelStyle: TextStyle(color: Colors.green),
-                    hintText: "이메일을 입력하세요",
-                    hintStyle: TextStyle(color: Colors.amber),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                        borderSide: BorderSide(width: 4, color: Colors.red)),
-                    enabledBorder: UnderlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                        borderSide:
-                            BorderSide(width: 1, color: Colors.lightBlue)),
-                    enabled: true,
-                    icon: Icon(Icons.search),
-                    prefixIcon: Icon(Icons.phone),
-                    suffixIcon: Icon(Icons.stacked_bar_chart),
+          title: Text("로그인 스크린", style: TextStyle(color: Colors.white)),
+          backgroundColor: Theme.of(context).colorScheme.primary,
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SizedBox(height: 40),
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.email)),
                 ),
-                keyboardType: TextInputType.emailAddress,
-                textInputAction: TextInputAction.search,
-                obscureText: false, // 값을 안보이게함
-                controller: _emailController,
-              ),
-              ElevatedButton(onPressed: (){print(_emailController.text);}, child: Text('클릭'))
-            ],
+                SizedBox(height: 16),
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.lock)),
+                ),
+                SizedBox(height: 24),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      textStyle: TextStyle(fontSize: 20),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20))),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 16),
+                    child: Text(
+                      '로그인',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontFamily: "Dohyeon"),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    '비밀번호 찾기',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Don\'t have an account?'),
+                    TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          '회원가입',
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Theme.of(context).colorScheme.primary),
+                        ))
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
