a433a139019851a3a7b494c3880bab731056df26
이관영 <71133204+stufit@users.noreply.github.com>
2023-10-24 23:15:13 +0900

    피드백 서베이 화면 구성하기1

--- lib/main.dart
+++ lib/main.dart
@@ -1,173 +1,103 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(FeedbackForm());
 }
 
-// ListView(seperator 추가)
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class FeedbackForm extends StatefulWidget {
+  const FeedbackForm({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<FeedbackForm> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
-    );
-  }
-}
+class _MyAppState extends State<FeedbackForm> {
+  final _formKey = GlobalKey<FormState>();
+  int? _rating = 0;
+  String _successMessage = '';
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("리스트목록")),
-      body: ListView.separated(
-        itemCount: 15,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text('Item번호 $index'),
-            subtitle: Text("Item번호 $index 설명"),
-            leading: CircleAvatar(
-              backgroundColor: Colors.amber,
-              child: Text(index.toString()),
-            ),
-          );
-        },
-        separatorBuilder: (context, index) {
-          return Card(
-            color: Colors.green,
-            child: Padding(
-              padding: EdgeInsets.all(8),
-              child: Text("Separator $index",
-                  style: TextStyle(color: Colors.purpleAccent)),
-            ),
-          );
-        },
-      ),
-    );
-  }
-}
-
-/* ListView version2
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("리스트목록")),
-      body: ListView.builder(
-        physics: BouncingScrollPhysics(), // 스크롤방식
-          itemCount: 15,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text('Item번호 $index'),
-              subtitle: Text("Item번호 $index 설명"),
-              leading: CircleAvatar(
-                backgroundColor: Colors.amber,
-                child: Text(index.toString()),
-              ),
-            );
-          }),
-    );
-  }
-}
-
- */
-
-/* ListView version 1
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("스투핏"),
+          title: Text("프로젝트1"),
         ),
-        body: ListView(
-          reverse: false,
+        body: Padding(
           padding: EdgeInsets.all(10),
-          children: [
-            ListTile(
-              leading: CircleAvatar(
-                backgroundColor: Colors.amber,
-                child: Text("1번"),
-              ),
-              title: Text("아이템1번"),
-              subtitle: Text("맥북"),
-              trailing: IconButton(
-                icon: Icon(Icons.more_horiz),
-                onPressed: () {
-                  print('클릭했다.');
-                },
-              ),
-            ),
-            ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: Colors.amber,
-                  child: Text("2번"),
-                ),
-                title: Text("아이템2번"),
-                subtitle: Text("아이패드"),
-                trailing: IconButton(
-                  icon: Icon(Icons.more_vert),
-                  onPressed: () {
-                    print('클릭했다.');
+          child: Form(
+            key: _formKey,
+            child: ListView(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(labelText: "이름"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "이름을 입력해주세요.";
+                    } else {
+                      return null;
+                    }
                   },
                 ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: "이메일"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "이메일을 입력해주세요.";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                ListTile(
+                  title: Text("우리의 앱 서비스는 몇점?"),
+                  trailing: Text('$_rating'),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: List<Widget>.generate(5, (index) {
+                    return Radio<int>(
+                      value: index + 1,
+                      groupValue: _rating,
+                      onChanged: (value) {
+                        setState(() {
+                          _rating = value;
+                        });
+                      },
+                    );
+                  }),
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        setState(() {
+                          _successMessage = "성공하였습니다.";
+                        });
+                      }else{
+                        setState(() {
+                          _successMessage="빈 공란이 있습니다.";
+                        });
+                      }
+                    },
+                    child: Text("제출")),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Center(
+                    child: Text(
+                      _successMessage,
+                      style: TextStyle(
+                          color: Colors.blueAccent,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14),
+                    ),
+                  ),
+                )
+              ],
             ),
-            ListTile(
-              leading: CircleAvatar(
-                backgroundColor: Colors.amber,
-                child: Text("3번"),
-              ),
-              title: Text("아이템3번"),
-              subtitle: Text("아이폰"),
-              trailing: IconButton(
-                icon: Icon(Icons.more_horiz),
-                onPressed: () {
-                  print('클릭했다.');
-                },
-              ),
-            ),
-
-          ],
+          ),
         ),
       ),
     );
   }
 }
- */
