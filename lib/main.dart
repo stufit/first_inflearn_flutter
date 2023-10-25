2ffbd86cafdc826b2a3f21a4e1d51bd5058ca4c5
이관영 <71133204+stufit@users.noreply.github.com>
2023-10-25 11:53:13 +0900

    이미지 갤러리 화면 구성하기1

--- lib/main.dart
+++ lib/main.dart
@@ -4,44 +4,6 @@ void main() {
   runApp(MyApp());
 }
 
-// pageView
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("페이지뷰"),
-        ),
-        body: PageView(
-          controller: PageController(
-            initialPage: 2,
-            viewportFraction: 0.7
-          ),
-          children: [
-            Container(
-              margin: EdgeInsets.all(10),color: Colors.red,
-            ),
-            Container(
-              margin: EdgeInsets.all(10),color: Colors.blueAccent,
-            ),
-            Container(
-              margin: EdgeInsets.all(10),color: Colors.green,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-
-
-
-
-/* GridView
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -49,27 +11,95 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("그리드뷰"),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: GridView.builder(
-            // scrollDirection: Axis.horizontal,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2, crossAxisSpacing: 8, mainAxisSpacing: 12),
-            itemCount: 10,
-            itemBuilder: (context, index) {
-              return Container(
-                color: Colors.purpleAccent,
-                child: Center(child: Text('테스트')),
-              );
-            },
-          ),
-        ),
-      ),
+      theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber)),
+      home: MyHomePage(),
     );
   }
 }
- */
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(
+            "프로젝트1",
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+          backgroundColor: Theme.of(context).colorScheme.primary),
+      body: Stack(
+        children: [
+          PageView(
+            children: [
+              _buildPage(
+                  imageUrl: "https://source.unsplash.com/random/3",
+                  userName: "Stufit",
+                  avatarUrl: "https://randomuser.me/api/portraits/men/60.jpg"),
+              _buildPage(
+                  imageUrl: "https://source.unsplash.com/random/2",
+                  userName: "WAF",
+                  avatarUrl: "https://randomuser.me/api/portraits/men/61.jpg"),
+              _buildPage(
+                  imageUrl: "https://source.unsplash.com/random/1",
+                  userName: "Gu",
+                  avatarUrl: "https://randomuser.me/api/portraits/men/62.jpg")
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPage(
+      {required String imageUrl,
+      required String userName,
+      required String avatarUrl}) {
+    return Stack(
+      children: [
+        Positioned.fill(
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Positioned(
+          bottom: 20,
+          left: 20,
+          child: Row(
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(avatarUrl),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Text(
+                userName,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    shadows: [
+                      Shadow(
+                      offset: Offset(1,1),
+                        blurRadius: 2,
+                        color: Colors.red.withOpacity(1)
+                    )]),
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
